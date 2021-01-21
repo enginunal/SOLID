12e8fe9d0877bac0257934b22d2d412a7d339e77
Engin ÜNAL <enginunal@gmail.com>
2021-01-21 17:09:14 +0300

    Create DIP_After.cs

---
+++ DIP_After.cs
@@ -0,0 +1,46 @@
+namespace DIP.After
+{
+    public class Copy
+    {
+        IReader _reader;
+        IWriter _writer;
+
+        public Copy(IReader reader, IWriter writer)
+        {
+            _reader = reader;
+            _writer = writer;
+        }
+        public void CopyTo()
+        {    
+            string copyText = _reader.ReadFrom();
+            _writer.WriteTo(copyText);
+        }
+    }
+
+    public interface IReader
+    {
+        public string ReadFrom();
+    }
+
+    public interface IWriter
+    {
+        public void WriteTo(string msg);
+    }
+
+    public class KeyboardReader : IReader
+    {
+        public string ReadFrom()
+        {
+            return "ReadFrom() function";
+        }
+    }
+
+    public class PrinterWriter : IWriter
+    {
+        public void WriteTo(string msg)
+        {
+            //...
+        }
+    }
+
+}
